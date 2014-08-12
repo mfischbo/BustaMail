7c055b366019ac5717693fa30a1c6f9515169a7a
Markus Fischboeck <m.fischboeck@googlemail.com>
2014-08-12 03:39:23 +0200

    applied model changes to initial install script

--- parent/bustamail-embedded/src/main/resources/install.sql
+++ parent/bustamail-embedded/src/main/resources/install.sql
@@ -8,8 +8,8 @@ insert into Security_Actor(id, addToChildren, addToFutureChildren, OrgUnit_id, U
 0xDED8F0C4D5F042269EFA8DC33337A36E, 1, 1, 0xAEF3B4393CA946DCA0E0CDB030F25A0C, 0x642430F616964205B1145D963E96CCFD);
 
 
-insert into Security_Actor_Permission(Actor_id, Permission_id) values (0xDED8F0C4D5F042269EFA8DC33337A36E, 0x3EA312EE637B4EE984A5446ED7CFA061);
-insert into Security_Actor_Permission(Actor_id, Permission_id) values (0xDED8F0C4D5F042269EFA8DC33337A36E, 0x2F2FE0C63B964893B80BDA85ADFFE8FE);
+insert into Security_Actor_Permission(Actor, Permission_id) values (0xDED8F0C4D5F042269EFA8DC33337A36E, 0x3EA312EE637B4EE984A5446ED7CFA061);
+insert into Security_Actor_Permission(Actor, Permission_id) values (0xDED8F0C4D5F042269EFA8DC33337A36E, 0x2F2FE0C63B964893B80BDA85ADFFE8FE);
 
 insert into Media_Directory (id, Owner_id, description, name, Parent_id) values (
 0x15F812FBC25C45ABAB5B1FDB7D2DCE33,
