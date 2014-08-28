ca4981623007ed73ae759190d923f2b18f0c7786
Markus Fischboeck <m.fischboeck@googlemail.com>
2014-08-28 18:44:28 +0200

    fixed some stuff

--- parent/bustamail-landingpage/src/main/java/de/mfischbo/bustamail/landingpage/domain/HTMLPage.java
+++ parent/bustamail-landingpage/src/main/java/de/mfischbo/bustamail/landingpage/domain/HTMLPage.java
@@ -2,6 +2,11 @@ package de.mfischbo.bustamail.landingpage.domain;
 
 import java.util.UUID;
 
+/**
+ * Interface for html pages like landing pages and static pages
+ * @author foobox
+ *
+ */
 public interface HTMLPage {
 
 	UUID getId();
