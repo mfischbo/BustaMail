903582bf7d3c6fcdd61d337de20fbcc2e223889b
Markus Fischboeck <m.fischboeck@googlemail.com>
2014-08-31 15:28:10 +0200

    removed properties file from configuration

--- parent/bustamail-media/src/main/java/de/mfischbo/bustamail/media/config/MediaConfiguration.java
+++ parent/bustamail-media/src/main/java/de/mfischbo/bustamail/media/config/MediaConfiguration.java
@@ -3,10 +3,8 @@ package de.mfischbo.bustamail.media.config;
 import org.apache.tika.Tika;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:bm-media.properties")
 public class MediaConfiguration {
 
 	@Bean
