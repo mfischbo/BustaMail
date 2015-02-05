1539720d8292025095efc3c598790f84cadc4763
Markus Fischboeck <m.fischboeck@googlemail.com>
2015-02-06 00:43:23 +0100

    added cleanup routine after a BatchMailWorker has had a RUS (rapid unscheduled shutdown)

--- parent/bustamail-mailer/src/main/java/de/mfischbo/bustamail/mailer/worker/JobFolderObserver.java
+++ parent/bustamail-mailer/src/main/java/de/mfischbo/bustamail/mailer/worker/JobFolderObserver.java
@@ -6,6 +6,8 @@ import java.io.FileFilter;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -26,6 +28,11 @@ public class JobFolderObserver {
 
 	private File	jobBasedir;
 	
+	private Logger  log = LoggerFactory.getLogger(getClass());
+
+	/* indicates whether this is the initial run of the observer */
+	private boolean	initialRun = true;
+	
 	private SMTPConfiguration fallbackConfig;
 	
 	@PostConstruct
@@ -37,16 +44,27 @@ public class JobFolderObserver {
 	
 	@Scheduled(fixedRate=30000)
 	void observeJobFolders() {
+		log.debug("Executing JobFolderObserver on directory {}", this.jobBasedir.getAbsolutePath());
 		File[] jobs = this.jobBasedir.listFiles(new FileFilter() {
 			@Override
 			public boolean accept(File j) {
 				if (j.isDirectory() && j.canRead()) {
 					File activationFile = new File(j.getAbsolutePath() + "/.activate");
 					File runningFile    = new File(j.getAbsoluteFile() + "/.running");
-					return (activationFile.exists() && !runningFile.exists());
+					
+					if (initialRun)
+						return activationFile.exists();
+					else
+						return (activationFile.exists() && !runningFile.exists());
 				} else return false;
 			}
 		});
+		
+		log.debug("Found {} potential matches", jobs.length);
+		if (initialRun) {
+			runInitialCleanup(jobs);
+			initialRun = false;
+		}
 		
 		// check if the mailer is enabled or not
 		Boolean enabled = env.getProperty("de.mfischbo.bustamail.mailer.enabled", Boolean.class);
@@ -58,6 +76,22 @@ public class JobFolderObserver {
 			for (File j : jobs) {
 				BatchMailWorker w = new BatchMailWorker(j, mapper, this.fallbackConfig);
 				w.execute();
+			}
+		}
+	}
+	
+	
+	/**
+	 * Runs a cleanup when the mailer has had an unexpected shutdown.
+	 * This method removes all .running file markers from the job directories
+	 * @param jobs
+	 */
+	private void runInitialCleanup(File[] jobs) {
+		for (File f : jobs) {
+			File marker = new File(f.getAbsolutePath() + "/.running");
+			if (marker.exists()) {
+				log.info("Removing .running marker from job directory {}", f.getAbsolutePath());
+				marker.delete();
 			}
 		}
 	}
